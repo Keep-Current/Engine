35eb3c4b90dc4230903d4d4cf9acbb35d9976c1a
Liad Magen <liad.magen@gmail.com>
2018-02-28 15:19:20 +0100

    added DB credentials placeholders in the settings file

--- src/common/settings.py
+++ src/common/settings.py
@@ -4,9 +4,15 @@ Settings contains different setting parametres to be used
 throughout the system
 """
 
-settings = {}
+# Database settings
 
-"""
-Database settings
-"""
-settings['DB_TYPE'] = "MongoDB"
+# Determines which DB to use 
+DB_TYPE = "MongoDB"
+# Credentials: the username for read-only access user
+DB_RO_USER = ""
+# Credentials: the password for read-only access user
+DB_RO_PASSWORD = ""
+# Credentials: the username for read-write access user
+DB_RW_USER = ""
+# Credentials: the password for read-write access user
+DB_RW_PASSWORD = ""
