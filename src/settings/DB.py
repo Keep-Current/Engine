9448254b5e6b0e0a09d9cc4a700b031c6ae7a894
Liad Magen <liad.magen@gmail.com>
2018-03-05 23:48:15 +0100

    settings

--- src/settings/DB.py
+++ src/settings/DB.py
@@ -3,10 +3,10 @@
 DB = dict(
     readonly = dict(
         username = 'KeepCurrent_1_RO',
-        password =  'o0yHmbjR3jYvecum'
+        password =  '<PASSWRORD>'
         ),
     readwrite = dict(
         username = 'KeepCurrent_1_RW',
-        password = 'Wxq3CMsJ05Vtg5IM'
+        password = '<PASSWORD>'
     )
 )
